72b8cf7de6ef5f9850a35a22cf09879975a58d0f
Alex Rocha <rocha.amc@gmail.com>
2023-08-20 19:07:46 -0700

    Implement removeExtraSpaces method that removes any extra whitespace characters

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { capitalize } from './methods/capitalize';
 export { allCaps } from './methods/allCaps';
 export { capitalizeWords } from './methods/capitalizeWords';
 export { capitalizeHeadline } from './methods/capitalizeHeadline';
+export { removeExtraSpaces } from './methods/removeExtraSpaces';
--- tests/test.ts
+++ tests/test.ts
@@ -1,4 +1,4 @@
-import { capitalize, allCaps, capitalizeWords, capitalizeHeadline } from '../src';
+import { capitalize, allCaps, capitalizeWords, capitalizeHeadline, removeExtraSpaces } from '../src';
 
 test('capitalize', () => {
   expect(capitalize('hello')).toBe('Hello');
@@ -18,4 +18,9 @@ test('capitalizeWords', () => {
 test('capitalizeHeadline', () => {
   expect(capitalizeHeadline('the most foo in bar')).toBe('The Most Foo in Bar');
   expect(capitalizeHeadline('in the rear with the gear')).toBe('In the Rear With the Gear');
+});
+
+test('removeExtraSpaces', () => {
+  expect(removeExtraSpaces('  hello     world!  ')).toBe('hello world!');
+  expect(removeExtraSpaces('\thello\f\v\rworld!  \n')).toBe('hello world!');
 });
