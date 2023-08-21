e12c9c8ec9ffcddadb3008c8f570db638388bd2d
Alex Rocha <rocha.amc@gmail.com>
2023-08-20 17:49:10 -0700

    Add allCaps tests

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,12 @@
-import { capitalize } from '../src'
+import { capitalize, allCaps } from '../src'
 
 test('capitalize', () => {
-  expect(capitalize('hello')).toBe('Hello')
-  expect(capitalize('hello world!')).toBe('Hello world!')
-})
+  expect(capitalize('hello')).toBe('Hello');
+  expect(capitalize('hello world!')).toBe('Hello world!');
+});
+
+test('allCaps', () => {
+  expect(allCaps('hello')).toBe('HELLO');
+  expect(allCaps('hello world!')).toBe('HELLO WORLD!');
+});
+
